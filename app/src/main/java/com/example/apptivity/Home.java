ac44971fb93373dded178f5a8f451f429879bb24
ThomasHengmith <s-thengm@haw-landshut.de>
2019-12-20 18:16:38 +0100

    - New activities - 2/3 home links working

--- app/src/main/java/com/example/apptivity/Home.java
+++ app/src/main/java/com/example/apptivity/Home.java
@@ -2,8 +2,11 @@ package com.example.apptivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static com.example.apptivity.PersonalInformation.INPUT_NAME;
@@ -11,12 +14,32 @@ import static com.example.apptivity.PersonalInformation.INPUT_NAME;
 
 public class Home extends AppCompatActivity {
     private TextView greetings;
+    private Button btFav;
+    private Button btSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         insertNameintoTextView();
+
+        btFav =  findViewById(R.id.btFav);
+        btFav.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openFavos();
+            }
+
+        });
+
+        btSearch =  findViewById(R.id.btSearch);
+        btSearch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSearch();
+            }
+
+        });
     }
 
     private void insertNameintoTextView(){
@@ -24,6 +47,15 @@ public class Home extends AppCompatActivity {
         String text = "Hallo "+mSharedPreferences.getString(INPUT_NAME, "")+"!";
         greetings =  findViewById(R.id.greetings);
         greetings.setText(text);
+    }
+    public void openSearch(){
+        Intent intent = new Intent(this, Search1.class);
+        startActivity(intent);
+    }
+
+    public void openFavos(){
+        Intent intent = new Intent(this, Favourites.class);
+        startActivity(intent);
     }
 
 }
