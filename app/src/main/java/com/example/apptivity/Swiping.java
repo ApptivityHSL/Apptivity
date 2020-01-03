df322e74b1e8e2fd9658a5ac12ce89f2e9f19bfd
ThomasHengmith <s-thengm@haw-landshut.de>
2020-01-03 23:09:37 +0100

    First Steps on Swiping

--- app/src/main/java/com/example/apptivity/Swiping.java
+++ app/src/main/java/com/example/apptivity/Swiping.java
@@ -7,13 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
 public class Swiping extends AppCompatActivity {
     private Button btBackHome;
+    private Button btSwipeUp;
+    private Button btSwipeLeft;
+    private Button btSwipeRight;
+    private ConnectFirebase connection = new ConnectFirebase();
+    private JSONObject activities = new Gson().fromJson(connection.getAllData("Test"), JSONObject.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swiping);
+
+        nextActivity();
 
         btBackHome =  findViewById(R.id.btBackHome);
         btBackHome.setOnClickListener(new View.OnClickListener(){
@@ -23,10 +34,49 @@ public class Swiping extends AppCompatActivity {
             }
 
         });
+
+        btSwipeUp =  findViewById(R.id.btSwipeUp);
+        btSwipeUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                swipeUp();
+            }
+
+        });
+
+        btSwipeLeft =  findViewById(R.id.btSwipeLeft);
+        btSwipeLeft.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                swipeLeft();
+            }
+
+        });
+
+        btSwipeRight =  findViewById(R.id.btSwipeRight);
+        btSwipeRight.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                swipeRight();
+            }
+
+        });
     }
 
     public void openHome(){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
+    }
+    public void swipeLeft(){
+
+    }
+    public void swipeRight(){
+
+    }
+    public void swipeUp(){
+
+    }
+    public void nextActivity(){
+
     }
 }
