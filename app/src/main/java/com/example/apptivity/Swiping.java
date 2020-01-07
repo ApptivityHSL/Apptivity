ac4b852da33697cfffee03627f5edc337b3c3eff
ThomasHengmith <s-thengm@haw-landshut.de>
2020-01-07 15:59:00 +0100

    Swiping Changes

--- app/src/main/java/com/example/apptivity/Swiping.java
+++ app/src/main/java/com/example/apptivity/Swiping.java
@@ -4,64 +4,103 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+import java.util.ArrayList;
+
 public class Swiping extends AppCompatActivity {
+
     private Button btBackHome;
-    private Button btSwipeUp;
-    private Button btSwipeLeft;
-    private Button btSwipeRight;
+    /*
     private ConnectFirebase connection = new ConnectFirebase();
     private JSONObject activities = new Gson().fromJson(connection.getAllData("Test"), JSONObject.class);
+    */
+
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swiping);
 
-        nextActivity();
+        al = new ArrayList<>();
+        al.add("php");
+        al.add("c");
+        al.add("python");
+        al.add("java");
+        al.add("html");
+        al.add("c++");
+        al.add("css");
+        al.add("javascript");
 
-        btBackHome =  findViewById(R.id.btBackHome);
-        btBackHome.setOnClickListener(new View.OnClickListener(){
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al);
+
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
-            public void onClick(View v){
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(Swiping.this, "Left!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(Swiping.this, "Right!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(Swiping.this, "Clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btBackHome = findViewById(R.id.btBackHome);
+        btBackHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 openHome();
-            }
-
-        });
-
-        btSwipeUp =  findViewById(R.id.btSwipeUp);
-        btSwipeUp.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                swipeUp();
-            }
-
-        });
-
-        btSwipeLeft =  findViewById(R.id.btSwipeLeft);
-        btSwipeLeft.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                swipeLeft();
-                nextActivity();
-            }
-
-        });
-
-        btSwipeRight =  findViewById(R.id.btSwipeRight);
-        btSwipeRight.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                swipeRight();
-                nextActivity();
             }
 
         });
@@ -71,15 +110,5 @@ public class Swiping extends AppCompatActivity {
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
-    public void swipeLeft(){
-
-    }
-    public void swipeRight(){
-
-    }
-    public void swipeUp(){
-
-    }
-    public void nextActivity(){
-    }
 }
+
