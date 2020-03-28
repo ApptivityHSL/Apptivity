509c952bf79f6fef6bb13d44cb652712184ca935
ThomasHengmith <s-thengm@haw-landshut.de>
2020-03-28 20:24:33 +0100

    Trying to show matches on Favourites.java

--- app/src/main/java/com/example/apptivity/Favourites.java
+++ app/src/main/java/com/example/apptivity/Favourites.java
@@ -3,18 +3,44 @@ package com.example.apptivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Set;
 
 public class Favourites extends AppCompatActivity {
 
+    public static final String MATCHES ="match";
     private Button btBackHome;
+    private Set<String> matches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
+
+
+        SharedPreferences mSharedPreferences = getSharedPreferences("activity_swiping", MODE_PRIVATE);
+        matches = mSharedPreferences.getStringSet(MATCHES, matches);
+        String[] matchesToView = new String[matches.size()];
+        int index = 0;
+        for (String str : matches)
+            matchesToView[index++] = str;
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        setContentView(linearLayout);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        for( int i = 0; i < matchesToView.length; i++ )
+        {
+            TextView textView = new TextView(this);
+            textView.setText(matchesToView[i]);
+            linearLayout.addView(textView);
+        }
+
 
         btBackHome =  findViewById(R.id.btBackToHome);
         btBackHome.setOnClickListener(new View.OnClickListener(){
