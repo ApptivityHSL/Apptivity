12c4fcffaf36385cf201e3f9f6a2827dab7d2901
alexa <alexander.fenzl@gmail.com>
2020-04-05 02:48:31 +0200

    Bundle wirft NullPointerException
    Zurück Button fertig

--- app/src/main/java/com/example/apptivity/ActivityOverview.java
+++ app/src/main/java/com/example/apptivity/ActivityOverview.java
@@ -2,11 +2,16 @@ package com.example.apptivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
 public class ActivityOverview extends AppCompatActivity {
+
+    private Button btBack;
 
 
     @Override
@@ -15,9 +20,26 @@ public class ActivityOverview extends AppCompatActivity {
         setContentView(R.layout.activity_overview);
 
 
+        btBack =  findViewById(R.id.btSwiping);
+        btBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSwiping();
+            }
+        });
+
+
 
     //    Bundle receiveTag = getIntent().getExtras();
     //    String activityID = receiveTag.getString("choosenActivity");
 
     }
+
+
+
+    public void openSwiping(){
+        Intent intent = new Intent(this, Swiping.class);
+        startActivity(intent);
+    }
+
 }
